7e89490e06b8dd6e96339cff615d78be5d28e110
liliya-pc <Miraj133@yandex.ru>
2022-02-16 17:12:22 +0500

    asd

--- app/src/main/java/com/example/myapplica/MyMovieListAdapter.kt
+++ app/src/main/java/com/example/myapplica/MyMovieListAdapter.kt
@@ -1,4 +1,32 @@
 package com.example.myapplica
 
-class MyMovieListAdapter(private val ) {
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplica.databinding.ItemMovieBinding
+
+class MyMovieListAdapter(
+    private val context: Context,
+    private val movieList: MutableList<ResponseData>
+) : RecyclerView.Adapter<MyMovieListAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        lateinit var binding: ItemMovieBinding
+        fun bind(listItem: ResponseData) {
+           binding.ivImageMovie
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
